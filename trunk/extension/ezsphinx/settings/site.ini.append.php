a870b61f74f1551188c01a1763344b34fc90cfa8
remdex <info@example.com>
2009-04-01 19:20:07 +0000

    Initial import of eZSphinx
    git-svn-id: http://svn.projects.ez.no/ezsphinx@3 afcbd8e4-6194-47a6-b9b4-e80e552c815d

---
+++ trunk/extension/ezsphinx/settings/site.ini.append.php
@@ -0,0 +1,13 @@
+<?php /*
+
+[SearchSettings]
+# eZ Sphinx overload search settings.
+ExtensionDirectories[]=ezsphinx
+SearchEngine=ezsphinx
+SearchViewHandling=template
+
+# Empty search is allowed by default for eZ Sphinx, since it does not
+# use extra resources compared to regular search.
+AllowEmptySearch=enabled
+
+*/ ?>
